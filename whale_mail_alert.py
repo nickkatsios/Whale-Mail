779183074fbb4ce2a7fabc24b792bfcfd51c45c8
NickKatsios <t8200071@aueb.gr>
2022-07-16 13:59:42 +0300

    Organize reqs

--- whale_mail_alert.py
+++ whale_mail_alert.py
@@ -5,79 +5,91 @@ import time
 import schedule
 import os
 
-# Get environment variable values. 
+# REQIREMENTS
+
+# Get environment variable values.
 key = os.environ.get("WHALE_ALERT_API_KEY")
 app_password = os.environ.get("WHALE_MAIL_APP_PASSWORD")
 
-# List all recipients of whale mail. 
-# Add any e-mail adress by appending it to the list.  
+# List all recipients of whale mail.
+# Add any e-mail adress by appending it to the list.
 recipients = ["whale.mail.alert@gmail.com"]
 
 # Interval between excecutions in seconds.
 interval = 120
 
-# Transaction value above witch a mail is sent. 
-threshold_value = 500000 
-# Call Whale Alert API to list all large tranactions (>500.000$) at a set interval time. 
+# Transaction value above witch a mail is sent.
+threshold_value = 500000
+
+# Your gmail adress to login to smtp server
+gmail_adress = "whale.mail.alert@gmail.com"
+
+# Running switch
+running = True
+
+# Call Whale Alert API to list all large tranactions (>500.000$) at a set interval time.
 # Transaction log starts from x (interval) secs ago and ends the moment we call the API (now)
-# in order to achieve live tranaction monitoring. 
+# in order to achieve live tranaction monitoring.
 def api_call():
 
-    # Parameters to add to request URL. 
+    # Parameters to add to request URL.
     payload = {
-    "api_key" : key,
-    "start" : int(time.time()) - interval,
-    "end" : int(time.time()) 
+        "api_key": key,
+        "start": int(time.time()) - interval,
+        "end": int(time.time())
     }
 
     print("Time Start: " + str(payload["start"]))
     print("Time End: " + str(payload["end"]))
     print()
     # API call
-    r = requests.get("https://api.whale-alert.io/v1/transactions" , params = payload)
+    r = requests.get(
+        "https://api.whale-alert.io/v1/transactions", params=payload)
     r_dict = r.json()
 
-    # Extract wantedd info from .json response 
+    # Extract wantedd info from .json response
     if int(r_dict["count"]) > 0:
         for transaction in r_dict["transactions"]:
             usd_value = int(transaction["amount_usd"])
-            if usd_value > threshold_value: 
+            if usd_value > threshold_value:
                 blockchain = transaction["blockchain"]
                 hash = transaction["hash"]
-                print(blockchain + ": " + str(usd_value) + " hash " + hash )
+                print(blockchain + ": " + str(usd_value) + " hash " + hash)
                 # Mail-Alert Recipients
-                send_mail_alert(blockchain , usd_value , hash)
+                send_mail_alert(blockchain, usd_value, hash)
 
 
 # Implement e-mail sending using smtp lib
-def send_mail_alert(blockchain , usd_value , hash):
-    
+def send_mail_alert(blockchain, usd_value, hash):
+
     mail = EmailMessage()
     mail["Subject"] = "Alert!"
     mail["From"] = "whale.mail.alert@gmail.com"
     mail["To"] = ", ".join(recipients)
 
-    mail.set_content("Large transaction on " + blockchain + " blockchain." + "USD value: " + str(usd_value))
-    
-    """ HTML MAIL TO BE IMPLEMENTED """
-    # mail.add_alternative("""\
-    # <!DOCTYPE html>
+    mail.set_content("Large transaction on " + blockchain +
+                     " blockchain." + "USD value: " + str(usd_value))
+
+    # html_content ="""<!DOCTYPE html>
     # <html>
     #     <body>
     #         <h1>Alert!</h1>
-    #         <img src="" alt="">
+    #         <p>Large trasaction on %s. USD value: %d </p>
+    #         <img src="https://github.com/ethereumclassic/ethereumclassic.github.io/blob/master/src/images/foundation.png?raw=true" alt="">
     #     </body>
-    # </html>
-    # """ , subtype = 'html')
+    # </html> 
+    # """%(blockchain, usd_value) 
+    # mail.add_alternative(html_content, subtype='html')
 
-    with smtplib.SMTP_SSL("smtp.gmail.com" , 465) as smtp:
-        smtp.login("whale.mail.alert@gmail.com" , app_password)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(gmail_adress, app_password)
         smtp.send_message(mail)
         print("Mail Sent!")
         print()
 
-# Run the script at set intervals 
+
+# Run the script at set intervals
 schedule.every(interval).seconds.do(api_call)
 
-while True:
+while running:
     schedule.run_pending()
